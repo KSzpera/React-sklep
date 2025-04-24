0e563e1037b0d355c42eaf7db140c4025eb2823b
Karolina <>
2025-04-24 17:24:50 +0200

    Login Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import ProductList from "./components/product/ProductList";
+import LoginForm from "./components/forms/LoginForm";
 
 const mockUser = {
   name: "John Doe",
@@ -11,11 +12,12 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Header user={mockUser} />
-      <ProductList />
-    </>
-  );
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {/* Header user={mockUser} />
+      <ProductList/> */}
+      <LoginForm />
+    </div>
+  )
 }
 
 export default App;
